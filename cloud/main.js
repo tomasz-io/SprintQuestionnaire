717d88fe323113758f93e93ad8192d4380103674
cancermonkey <tomasz.stefaniak.12@ucl.ac.uk>
2015-10-26 12:58:28 +0100

    minor changes

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,33 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 
+var mandrill = require("mandrill");
+mandrill.initialize("yEPF9THiZOmKolJaiNMPog");
+
+var myMandrillFunction = function(email) {
+  mandrill.sendEmail({
+    message: {
+      text: "Hello World!",
+      subject: "Using Cloud Code and Mandrill is great!",
+      from_email: "tomasz.s@numa.co",
+      from_name: "Cloud Code",
+      to: [
+        {
+          email: email,
+          name: "Your Name"
+        }
+      ]
+    },
+    async: true
+  });
+
+
+  /*, {
+    success: function(httpResponse) { response.success("Email sent!"); },
+    error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
+  });*/
+}
+
 var separateTags = function(string) {
   //this returns an array of words that were previously separated by commas
   var arr = string.split(",");
@@ -13,7 +40,6 @@ var separateTags = function(string) {
 }
 
 var compareScores = function(a,b) {
-  console.log(a.FitScore)
   if (a.FitScore < b.FitScore)
     return 1;
   if (a.FitScore > b.FitScore)
@@ -55,8 +81,94 @@ var getFitScore = function(startupTags, evaluatorTags) {
   return fitScore;
 }
 
+var sendEmail = function(email) {
 
-Parse.Cloud.define("hello", function(request, response) {
+  Parse.Cloud.httpRequest({
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json',
+      },
+      url: 'https://mandrillapp.com/api/1.0/messages/send-template.json',
+      body:{
+          "key": "yEPF9THiZOmKolJaiNMPog",
+          "template_name": "<Insert Template Name>",
+          "template_content": [{
+              "name": "example name",
+              "content": "example content" // Name and Content are required even though they are ignored
+              }],
+          "message": {
+              "to": [
+                  {
+                      "email": email,
+                   }
+                    ]
+      }},
+      success: function(httpResponse) {
+              console.log(httpResponse);
+              },
+          error: function(httpResponse) {
+              console.error(httpResponse);
+          }
+      });
+}
+
+Parse.Cloud.job("getUniqueTags", function(request, status) {
+
+  // Query for all upvotes
+  //NOT FINISHED
+  var Startups = Parse.Object.extend("Startups");
+  var query = new Parse.Query(Startups);
+  var allTags =[];
+
+  query.each(function(startup) {
+      var tagsString = startup.get("Tags");
+
+      if(tagsString != null) {
+        var tags = separateTags(tagsString);
+      } else {
+        var tags = [];
+      }
+
+      allTags = allTags.concat(tags);
+
+  }).then(function() {
+      console.log(allTags);
+      allTags = uniq_fast(allTags);
+      console.log(allTags);
+
+  }).then(function() {
+      status.success("getUniqueTags success");
+  }, function(error) {
+      status.error("getUniqueTags something went wrong.");
+  });
+});
+
+Parse.Cloud.define("emailCheck", function(request, response) {
+
+  var email = request.params.email;
+  var Evaluators = Parse.Object.extend("Evaluators");
+  var query = new Parse.Query(Evaluators);
+  query.equalTo("Email", email);
+
+  query.find().then(function(results) {
+
+      var emailExists = false
+      if (results.length > 0) {
+          emailExists = true;
+      }
+      return emailExists;
+
+  }).then(function(result) {
+      console.log("email : " + result);
+      response.success(result);
+  }, function(error) {
+      response.error('emailCheck error ' + error);
+  });
+
+
+});
+
+Parse.Cloud.define("getStartups", function(request, response) {
 
   var Startups = Parse.Object.extend("Startups");
 
@@ -64,7 +176,6 @@ Parse.Cloud.define("hello", function(request, response) {
   var needsBiz = new Parse.Query(Startups);
   needsBiz.doesNotExist("Biz");
   //needsBiz.equalTo("Biz", "test");
-
 
   //Startups that need Product evaluator
   var needsProduct = new Parse.Query(Startups);
@@ -82,16 +193,21 @@ Parse.Cloud.define("hello", function(request, response) {
 
   var arr = [];
 
+  var evaluatorIndustry = request.params.industry;
+  //console.log("industry" + evaluatorIndustry);
   var evaluatorTagsString = request.params.tags;
   var evaluatorTags = separateTags(evaluatorTagsString);
-
+  console.log(evaluatorTags);
+  evaluatorTags = evaluatorTags.concat(evaluatorIndustry); //we're using the industries as just regular tags
+  console.log(evaluatorTags);
+  console.log(evaluatorTags[0]);
 
   var bestFitScore = 0;
 
   mainQuery.each(function(startup){
 
+    var id = startup.id;
     var name = startup.get("Name");
-    var id = startup.get("ApplicationID");
     var biz = startup.get("Biz");
     var product = startup.get("Product");
     var tech = startup.get("Tech");
@@ -104,12 +220,6 @@ Parse.Cloud.define("hello", function(request, response) {
     var startupTags = separateTags(startupTags); //startupTags is a comma separated string of tags, need to be converted to an array first
     var fitScore = getFitScore(startupTags, evaluatorTags);
 
-    if (name == "Powize Team") {
-
-      console.log(fitScore);
-      console.log(startupTags);
-
-    }
 
     if(!checkIfEmailInString(biz)) {
       biz = 0;
@@ -122,8 +232,8 @@ Parse.Cloud.define("hello", function(request, response) {
     }
 
     arr.push({
-    "Name" : name,
     "Id" : id,
+    "Name" : name,
     "Biz" : biz,
     "Product" : product,
     "Tech" : tech,
@@ -141,5 +251,51 @@ Parse.Cloud.define("hello", function(request, response) {
     response.error(error.code + " : " + error.message);
   });
 
-//  response.success("Hello world! " + request.params.name);
+});
+
+
+Parse.Cloud.define("submit", function(request, response) {
+
+  var Startups = Parse.Object.extend("Startups");
+
+  var email = request.params.email;
+
+  var biz = request.params.biz;
+  var product = request.params.product;
+  var tech = request.params.tech;
+
+  console.log("biz : " + biz);
+  console.log("prod : " + product);
+  console.log("tech : " + tech);
+  console.log("email : " + email);
+
+  var bizQuery = new Parse.Query(Startups);
+  var productQuery = new Parse.Query(Startups);
+  var techQuery = new Parse.Query(Startups);
+
+  bizQuery.containedIn("objectId", biz);
+  productQuery.containedIn("objectId", product);
+  techQuery.containedIn("objectId", tech);
+
+  bizQuery.each(function(startup){
+    var name = startup.get("Name");
+    console.log("name : " + name);
+    startup.set("Biz", email);
+    startup.save();
+  });
+
+  productQuery.each(function(startup){
+    //var name = startup.get("Name");
+    startup.set("Product", email);
+    startup.save();
+  });
+
+  techQuery.each(function(startup){
+    //var name = startup.get("Name");
+    startup.set("Tech", email);
+    startup.save();
+  });
+
+  //myMandrillFunction(email);
+
 });
