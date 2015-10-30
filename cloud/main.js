ac5e2817444ff3fa7982d040496ae769d2eb95fb
tomasz-io <tomasz.stefaniak.12@ucl.ac.uk>
2015-10-30 10:44:03 +0100

    backend for checkboxes

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@ var separateTags = function(string) {
   var arr = string.split(",");
   var arrayLength = arr.length;
   for (var i = 0; i < arrayLength; i++) {
-    arr[i] = [arr[i].trim().toLowerCase()]; //remove leading and trailing whitespace
+    arr[i] = arr[i].trim().toLowerCase(); //remove leading and trailing whitespace
   }
   return arr
 }
@@ -67,67 +67,76 @@ var getFitScore = function(startupTags, evaluatorTags) {
   return fitScore;
 }
 
-var sendEmail = function(email) {
+// Parse.Cloud.beforeSave("Tags", function(request, response) {
+//
+//     console.log("beforeSave Tags");
+//
+//     var Tags = Parse.Object.extend("Tags");
+//     var query = new Parse.Query(Tags);
+//     query.equalTo("name", request.object.get("name"));
+//     query.first({
+//       success: function(object) {
+//         if (object) {
+//           response.error("beforeSave Tag : A tag with this name already exists.");
+//         } else {
+//           response.success();
+//         }
+//       },
+//       error: function(error) {
+//         response.error("beforeSave Tag : Could not validate uniqueness for this Tag object.");
+//       }
+//     });
+// });
 
-  Parse.Cloud.httpRequest({
-      method: 'POST',
-      headers: {
-       'Content-Type': 'application/json',
-      },
-      url: 'https://mandrillapp.com/api/1.0/messages/send-template.json',
-      body:{
-          "key": "yEPF9THiZOmKolJaiNMPog",
-          "template_name": "<Insert Template Name>",
-          "template_content": [{
-              "name": "example name",
-              "content": "example content" // Name and Content are required even though they are ignored
-              }],
-          "message": {
-              "to": [
-                  {
-                      "email": email,
-                   }
-                    ]
-      }},
-      success: function(httpResponse) {
-              console.log(httpResponse);
-              },
-          error: function(httpResponse) {
-              console.error(httpResponse);
-          }
-      });
-}
 
 Parse.Cloud.job("getUniqueTags", function(request, status) {
 
   // Query for all upvotes
   //NOT FINISHED
+  var Tags = Parse.Object.extend("Tags");
+  var tagQuery = new Parse.Query(Tags);
+  tagQuery.equalTo("name", "All Tags"); //the class only contains one object, called All Tags
+
   var Startups = Parse.Object.extend("Startups");
   var query = new Parse.Query(Startups);
-  var allTags =[];
+
+  var allTags =[]; //array with all tags (not unique)
 
   query.each(function(startup) {
-      var tagsString = startup.get("Tags");
 
+      //Get tags from each startup and create array of tags
+      var tagsString = startup.get("tags");
       if(tagsString != null) {
         var tags = separateTags(tagsString);
       } else {
         var tags = [];
       }
-
       allTags = allTags.concat(tags);
 
   }).then(function() {
-      console.log(allTags);
-      allTags = uniq_fast(allTags);
-      console.log(allTags);
+
+      console.log("count 1 : " + allTags.length);
+      allTags = allTags.filter(function(item, pos) {
+        return allTags.indexOf(item) == pos;
+      });
+      console.log("count 2 : " + allTags.length);
+
+      console.log("allTags : " + allTags);
+      return tagQuery.first();
+
+  }).then(function(tag) {
+
+      tag.set("tagsArray", allTags);
+      return tag.save();
 
   }).then(function() {
+      console.log("calling success");
       status.success("getUniqueTags success");
   }, function(error) {
       status.error("getUniqueTags something went wrong.");
   });
 });
+
 
 Parse.Cloud.define("emailCheck", function(request, response) {
 
@@ -153,6 +162,7 @@ Parse.Cloud.define("emailCheck", function(request, response) {
 
 
 });
+
 
 Parse.Cloud.define("getAssignments", function(request, response) {
 
@@ -202,44 +212,29 @@ Parse.Cloud.define("getAssignments", function(request, response) {
 
 Parse.Cloud.define("getStartups", function(request, response) {
 
-  var Startups = Parse.Object.extend("Startups");
 
-  //Startups that need Business evaluator
-  var needsBiz = new Parse.Query(Startups);
-  needsBiz.doesNotExist("biz");
+  var evaluatorExpertise = request.params.expertise;
 
-  //Startups that need Product evaluator
-  var needsProduct = new Parse.Query(Startups);
-  needsProduct.doesNotExist("product");
-
-  //Startups that need Tech evaluator
-  var needsTech = new Parse.Query(Startups);
-  needsTech.doesNotExist("tech");
-
-
-  //Main query
-  var mainQuery = Parse.Query.or(needsBiz, needsProduct, needsTech);
-
-  var arr = [];
+  console.log("expertise : " + evaluatorExpertise);
 
   var evaluatorIndustry = request.params.industry;
-  //console.log("industry" + evaluatorIndustry);
   var evaluatorTagsString = request.params.tags;
   var evaluatorTags = separateTags(evaluatorTagsString);
-  console.log(evaluatorTags);
   evaluatorTags = evaluatorTags.concat(evaluatorIndustry); //we're using the industries as just regular tags
-  console.log(evaluatorTags);
-  console.log(evaluatorTags[0]);
+
+
+  var Startups = Parse.Object.extend("Startups");
+  var query = new Parse.Query(Startups);
+  query.doesNotExist(evaluatorExpertise);
+
+  var startups = [];
 
   var bestFitScore = 0;
 
-  mainQuery.each(function(startup){
+  query.each(function(startup){
 
     var id = startup.id;
     var name = startup.get("name");
-    var biz = startup.get("biz");
-    var product = startup.get("product");
-    var tech = startup.get("tech");
     var tagline = startup.get("tagline");
     var startupTags = startup.get("tags");
 
@@ -247,29 +242,23 @@ Parse.Cloud.define("getStartups", function(request, response) {
       startupTags = "placeholder";
     }
 
+    //It could be a good idea to store startup tags as an array instead of a string
+    //Wouldn't have to convert them to array every time
     var startupTags = separateTags(startupTags); //startupTags is a comma separated string of tags, need to be converted to an array first
     var fitScore = getFitScore(startupTags, evaluatorTags);
 
-
-//javascript ternary operator
-     biz = (biz) ? 'has business evaluator' : 0;
-     product = (product) ? 'has product evaluator' : 0;
-     tech = (tech) ? 'has tech evaluator' : 0;
-
-    arr.push({
+    startups.push({
     "Id" : id,
     "Name" : name,
-    "Biz" : biz,
-    "Product" : product,
-    "Tech" : tech,
     "Tagline" : tagline,
     "FitScore" : fitScore
     });
 
   }).then(function(results){
 
-    arr = arr.sort(compareScores);
-    response.success(arr.slice(0,10));
+    startups = startups.sort(compareScores);
+    response.success(startups);
+    //response.success(arr.slice(0,10));
 
   }, function(error) {
     response.error(error.code + " : " + error.message);
@@ -285,16 +274,12 @@ Parse.Cloud.define("submit", function(request, response) {
 
   var email = request.params.email;
 
-  var biz = request.params.biz;
-  var product = request.params.product;
-  var tech = request.params.tech;
-  var allChoices = biz.concat(product, tech);
-  var allNames = [];
+  var selected = request.params.selected;
+  var expertise = request.params.expertise;
+  var allNames = []; //where we store the names of all startups assigned to evaluator
   var evaluator;
 
-  console.log("biz : " + biz);
-  console.log("prod : " + product);
-  console.log("tech : " + tech);
+  console.log("expertise : " + expertise);
   console.log("email : " + email);
 
   var evaluatorQuery = new Parse.Query(Evaluators);
@@ -302,46 +287,38 @@ Parse.Cloud.define("submit", function(request, response) {
 
   evaluatorQuery.find().then(function(results){
 
-    if ( results.length == 0 ) {
-      console.error("no such evaluator");
-    } else if (results.length > 0) {
-      evaluator = results[0];
-    }
+      if ( results.length == 0 ) {
+        console.error("no such evaluator");
+      } else if (results.length > 0) {
+        evaluator = results[0];
+      }
 
-    var query = new Parse.Query(Startups);
-    query.containedIn("objectId", allChoices);
-    return query.find();
+      var startupQuery = new Parse.Query(Startups);
+      startupQuery.containedIn("objectId", selected);
+      return startupQuery.find();
+
   }).then(function(results){
 
     for(var i=0, len=results.length; i < len; i++){
       var startup = results[i];
       var id = startup.id;
       name = startup.get("name");
-
-//Check if it's a biz, product or tech evaluator
-      if(isInArray(id, biz)){
-        startup.set("biz", evaluator);  //pointer
-      } else if(isInArray(id, product)){
-        startup.set("product", evaluator); //pointer
-      }else if(isInArray(id, tech)){
-        startup.set("tech", evaluator);  //pointer
-      } else {
-        console.error("startup is in none of the arrays");
-      }
+      startup.set(expertise, evaluator);  //pointer
       startup.save();
       allNames.push(name);
     }
 
   }).then(function(){
 
-    console.log("allNames: " + allNames);
-    //Create new assignment
-    var Assignments = Parse.Object.extend("Assignments");
-    var assignment = new Assignments();
-    assignment.set("evaluator", evaluator); //pointer
-    assignment.set("startupNames", allNames); //array
-    assignment.set("email", email); //string
-    assignment.set("assigned", false);
+      console.log("allNames: " + allNames);
+      //Create new assignment
+      var Assignments = Parse.Object.extend("Assignments");
+      var assignment = new Assignments();
+      assignment.set("evaluator", evaluator); //pointer
+      assignment.set("startupNames", allNames); //array
+      assignment.set("email", email); //string
+      assignment.set("expertise", expertise); //pointer
+      assignment.set("assigned", false);
 
     return assignment.save();
   }).then(function(result) {
@@ -372,3 +349,82 @@ Parse.Cloud.define("submitAssignments", function(request, response) {
   });
 
 });
+
+
+// Parse.Cloud.define("getStartups", function(request, response) {
+//
+//   var Startups = Parse.Object.extend("Startups");
+//
+//   //Startups that need Business evaluator
+//   var needsBiz = new Parse.Query(Startups);
+//   needsBiz.doesNotExist("biz");
+//
+//   //Startups that need Product evaluator
+//   var needsProduct = new Parse.Query(Startups);
+//   needsProduct.doesNotExist("product");
+//
+//   //Startups that need Tech evaluator
+//   var needsTech = new Parse.Query(Startups);
+//   needsTech.doesNotExist("tech");
+//
+//
+//   //Main query
+//   var mainQuery = Parse.Query.or(needsBiz, needsProduct, needsTech);
+//
+//   var arr = [];
+//
+//   var evaluatorIndustry = request.params.industry;
+//   //console.log("industry" + evaluatorIndustry);
+//   var evaluatorTagsString = request.params.tags;
+//   var evaluatorTags = separateTags(evaluatorTagsString);
+//   console.log(evaluatorTags);
+//   evaluatorTags = evaluatorTags.concat(evaluatorIndustry); //we're using the industries as just regular tags
+//   console.log(evaluatorTags);
+//   console.log(evaluatorTags[0]);
+//
+//   var bestFitScore = 0;
+//
+//   mainQuery.each(function(startup){
+//
+//     var id = startup.id;
+//     var name = startup.get("name");
+//     var biz = startup.get("biz");
+//     var product = startup.get("product");
+//     var tech = startup.get("tech");
+//     var tagline = startup.get("tagline");
+//     var startupTags = startup.get("tags");
+//
+//     if(startupTags == null) {
+//       startupTags = "placeholder";
+//     }
+//
+//     var startupTags = separateTags(startupTags); //startupTags is a comma separated string of tags, need to be converted to an array first
+//     var fitScore = getFitScore(startupTags, evaluatorTags);
+//
+//
+// //javascript ternary operator
+//      biz = (biz) ? 'has business evaluator' : 0;
+//      product = (product) ? 'has product evaluator' : 0;
+//      tech = (tech) ? 'has tech evaluator' : 0;
+//
+//     arr.push({
+//     "Id" : id,
+//     "Name" : name,
+//     "Biz" : biz,
+//     "Product" : product,
+//     "Tech" : tech,
+//     "Tagline" : tagline,
+//     "FitScore" : fitScore
+//     });
+//
+//   }).then(function(results){
+//
+//     arr = arr.sort(compareScores);
+//     response.success(arr);
+//     //response.success(arr.slice(0,10));
+//
+//   }, function(error) {
+//     response.error(error.code + " : " + error.message);
+//   });
+//
+// });
