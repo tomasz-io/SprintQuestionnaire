89fa3eb302253ea8e4744805c5c7fc654dd16854
tomasz-io <tomasz.stefaniak.12@ucl.ac.uk>
2015-12-23 11:45:30 +0100

    finished basic score calculation based on weights

--- cloud/evaluation.js
+++ cloud/evaluation.js
@@ -1,10 +1,94 @@
-var evaluatorsNumber = 50;
+var utilities = require('cloud/utilityFunctions.js');
+
+var evaluatorsNumber = 50; //number of evaluators that evaluated startups
+var reqEvaluators = 3; //Number of evaluators require per startup
 
 var weight_teamrating;
 var weight_customerKnowledge;
 var weight_opportunityMarket;
 var weight_industryExperience;
 var weight_competAdvantage;
+
+
+function getConfig() {
+
+  var promise = new Parse.Promise();
+  return Parse.Config.get().then(function(config){
+    weight_teamrating = config.get("weight_teamrating");
+    weight_customerKnowledge = config.get("weight_customerKnowledge");
+    weight_opportunityMarket = config.get("weight_opportunityMarket");
+    weight_industryExperience = config.get("weight_industryExperience");
+    weight_competAdvantage = config.get("weight_competAdvantage");
+    console.log("weight_competAdvantage : " + weight_competAdvantage);
+    promise.resolve();
+  });
+
+  return promise;
+}
+
+Parse.Cloud.job("calculateScores", function(request, status) {
+
+  var StartupScores = Parse.Object.extend("StartupScores");
+  var query = new Parse.Query(StartupScores);
+  query.limit(1000);
+
+  var promise = Parse.Promise.as();
+
+  getConfig().then(function(){
+    return query.find();
+  }).then(function(results) {
+
+    var allScores = Array();
+    for(var i=0, len=results.length; i < len; i++){
+
+      var eval = results[i];
+
+      var teamName = eval.get("Name");
+
+      var allTeam = eval.get("AllTeam");
+      var allCustomer = eval.get("AllCustomer");
+      var allMarket = eval.get("AllMarket");
+      var allIndustry = eval.get("AllIndustry");
+      var allAdvantage = eval.get("AllAdvantage");
+
+      var avgTeam = utilities.averageArray(allTeam);
+      var avgCustomer = utilities.averageArray(allCustomer);
+      var avgMarket = utilities.averageArray(allMarket);
+      var avgIndustry = utilities.averageArray(allIndustry);
+      var avgAdvantage = utilities.averageArray(allAdvantage);
+
+      var numaScore = weight_teamrating*avgTeam + weight_customerKnowledge*avgCustomer + weight_opportunityMarket*avgMarket + avgIndustry*weight_industryExperience + avgAdvantage*weight_competAdvantage;
+
+      eval.set("AdjustedScore", numaScore);
+
+      // if (i % 10 == 0) {
+      //   console.log(teamName + " : " + numaScore);
+      //   console.log(eval.get("AdjustedScore"));
+      // }
+
+      if(eval.get("AdjustedScore")) {
+
+        if(allTeam.length != reqEvaluators || allCustomer.length != reqEvaluators
+          || allMarket.length != reqEvaluators || allIndustry.length != reqEvaluators
+          || allAdvantage.length != reqEvaluators) {
+
+          console.log(teamName + " : " + numaScore);
+        }
+
+        allScores.push(eval);
+      }
+    }
+
+    console.log(allScores);
+
+    return Parse.Object.saveAll(allScores);
+
+  }).then(function() {
+    status.success("calculateScores success");
+  }, function(error) {
+    status.error();
+  });
+});
 
 Parse.Cloud.job("importScores", function(request, status) {
 
@@ -19,7 +103,7 @@ Parse.Cloud.job("importScores", function(request, status) {
 
   query.find().then(function(results) {
 
-    var allEvals = Array();
+    var allScores = Array();
     for(var i=0, len=results.length; i < len; i++){
 
       var eval = results[i];
@@ -67,14 +151,20 @@ Parse.Cloud.job("importScores", function(request, status) {
         // console.log(name + " competitive advantage : " + competAdvantage);
 
       }
-      eval.set("AllTeam", allTeam);
-      eval.set("AllCustomer", allCustomer);
-      eval.set("AllMarket", allMarket);
-      eval.set("AllIndustry", allIndustry);
-      eval.set("AllAdvantage", allAdvantage);
-      allEvals.push(eval);
+
+      var StartupScores = Parse.Object.extend("StartupScores");
+      var startupScore = new StartupScores();
+
+      startupScore.set("Name", name);
+      startupScore.set("AllTeam", allTeam);
+      startupScore.set("AllCustomer", allCustomer);
+      startupScore.set("AllMarket", allMarket);
+      startupScore.set("AllIndustry", allIndustry);
+      startupScore.set("AllAdvantage", allAdvantage);
+      allScores.push(startupScore);
     }
-    return Parse.Object.saveAll(allEvals);
+    console.log(allScores);
+    return Parse.Object.saveAll(allScores);
   }).then(function() {
       console.log("calling success");
       status.success("importScores success");
--- cloud/utilityFunctions.js
+++ cloud/utilityFunctions.js
@@ -72,6 +72,15 @@ module.exports = {
           }
       }
       return r;
+  },
+
+  averageArray: function(arr) {
+    var total = 0;
+    for(var i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    var avg = total / arr.length;
+    return avg;
   }
 
 }
